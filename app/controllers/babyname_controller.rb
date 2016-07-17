f2bc3abec55c922ed60a880adcfb282695591a3f
saimaroju <marojusaieswar@gmail.com>
2016-07-17 12:08:34 +0530

    sai - removed babynames ctrl

--- app/controllers/babyname_controller.rb
+++ app/controllers/babyname_controller.rb
@@ -4,61 +4,61 @@ class BabynameController < ApplicationController
 
   def namecorrection
   #params dob and fullname
-  	@date = params[:your_dob][:day].to_i
-    @birth,@fate = luckynum
-    @hash = {"A" => 1, "I" => 1, "J" => 1, "Q" => 1, "Y" => 1, "B" => 2, "K" => 2, "R" => 2, "c" => 3, "G" => 3, "L" => 3, "S" => 3, "D" => 4, "M" => 4, "T" => 4,"E" => 5, "H" => 5, "N" => 5, "X" => 5, "U" => 6, "V" => 6, "W" => 6, "O" => 7, "Z" => 7, "P" => 8, "F" => 9}
+  # 	@date = params[:your_dob][:day].to_i
+  #   @birth,@fate = luckynum
+  #   @hash = {"A" => 1, "I" => 1, "J" => 1, "Q" => 1, "Y" => 1, "B" => 2, "K" => 2, "R" => 2, "c" => 3, "G" => 3, "L" => 3, "S" => 3, "D" => 4, "M" => 4, "T" => 4,"E" => 5, "H" => 5, "N" => 5, "X" => 5, "U" => 6, "V" => 6, "W" => 6, "O" => 7, "Z" => 7, "P" => 8, "F" => 9}
      
-   # hash = {"A" => 1, "I" => 1, "J" => 1, "Q" => 1, "Y" => 1, "B" => 2, "K" => 2, "R" => 2, "c" => 3, "G" => 3, "L" => 3, "S" => 3, "D" => 4, "M" => 4, "T" => 4,"E" => 5, "H" => 5, "N" => 5, "X" => 5, "U" => 6, "V" => 6, "W" => 6, "O" => 7, "Z" => 7, "P" => 8, "F" => 9}
-   arr1 = []
-   arr2 = []
-   fullname = params[:fullname]
-   va = params[:fullname].gsub(/[^a-zA-Z]/, '')
-   va.each_char do|e|  
-     arr1 << hash["#{e}"]
-   end
-   arr2 << fullname.split("").map{|x| x.to_i}
+  #  # hash = {"A" => 1, "I" => 1, "J" => 1, "Q" => 1, "Y" => 1, "B" => 2, "K" => 2, "R" => 2, "c" => 3, "G" => 3, "L" => 3, "S" => 3, "D" => 4, "M" => 4, "T" => 4,"E" => 5, "H" => 5, "N" => 5, "X" => 5, "U" => 6, "V" => 6, "W" => 6, "O" => 7, "Z" => 7, "P" => 8, "F" => 9}
+  #  arr1 = []
+  #  arr2 = []
+  #  fullname = params[:fullname]
+  #  va = params[:fullname].gsub(/[^a-zA-Z]/, '')
+  #  va.each_char do|e|  
+  #    arr1 << hash["#{e}"]
+  #  end
+  #  arr2 << fullname.split("").map{|x| x.to_i}
    
-   @result =  arr1.zip(arr2).flatten.compact.inject(0, :+)
-   while result > 10
-     @result = result.to_s.split("").map(&:to_i).inject(0, :+)
-   end
+  #  @result =  arr1.zip(arr2).flatten.compact.inject(0, :+)
+  #  while result > 10
+  #    @result = result.to_s.split("").map(&:to_i).inject(0, :+)
+  #  end
    
-  end
+  # end
 
-  if @birth == @result == @fate
-    @report = "matched"
+  # if @birth == @result == @fate
+  #   @report = "matched"
 
-  elsif @birth
+  # elsif @birth
     
-  else
+  # else
     
-  end
+  # end
   
 
-  end
+  # end
 
 
 
-    case @birth
-      when 1,10,19,28
+  #   case @birth
+  #     when 1,10,19,28
     	  
-      when 2,11,20,29
+  #     when 2,11,20,29
         
-      when 3,12,21,30
+  #     when 3,12,21,30
 
-      when 4,13,22,31
+  #     when 4,13,22,31
 
-      when 5,14,23
+  #     when 5,14,23
 
-      when 6,15,24
+  #     when 6,15,24
 
-      when 7,16,25
+  #     when 7,16,25
 
-      when 8,17,26
+  #     when 8,17,26
         
-      when 9,18,27  
+  #     when 9,18,27  
          
-    end
+  #   end
   end
 
 private 
