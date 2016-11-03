53744e2ada477cf9feb32a74d1a2d3c52dd4f337
VIZAG MEN <vizagcity2012@gmail.com>
2016-11-03 23:31:26 +0530

    Prathuish - added new design for pandit :

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  # before_action :configure_devise_permitted_parameters, if: :devise_controller?
   helper_method :is_admin?
 
   protected
 
-  def configure_devise_permitted_parameters
-    registration_params = [:name, :email, :password, :password_confirmation, :aadhar_card, :phone, :reference, :address, :avatar,:gender, :serial_no, :dob, :role]
-
-    if params[:action] == 'update'
-      devise_parameter_sanitizer.for(:account_update) {
-        |u| u.permit(registration_params << :current_password)
-      }
-    elsif params[:action] == 'create'
-      devise_parameter_sanitizer.for(:sign_up) {
-        |u| u.permit(registration_params)
-      }
-    end
-end
+#   def configure_devise_permitted_parameters
+#     registration_params = [:name, :email, :password, :password_confirmation, :aadhar_card, :phone, :reference, :address, :avatar,:gender, :serial_no, :dob, :role]
+#
+#     if params[:action] == 'update'
+#       devise_parameter_sanitizer.for(:account_update) {
+#         |u| u.permit(registration_params << :current_password)
+#       }
+#     elsif params[:action] == 'create'
+#       devise_parameter_sanitizer.for(:sign_up) {
+#         |u| u.permit(registration_params)
+#       }
+#     end
+# end
 
 def after_sign_up_path_for(resource)
   dashboard_index_path
