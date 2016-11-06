3134b4a6c8e055c508dbbbde0ef245d4b6a4beea
VIZAG MEN <vizagcity2012@gmail.com>
2016-11-06 14:13:44 +0530

    pandit index page added

--- app/controllers/pandits_controller.rb
+++ app/controllers/pandits_controller.rb
@@ -1,36 +1,37 @@
 class PanditsController < ApplicationController
-	def index
-# @pandits = Pandit.all.includes(:user)
-
-		@pandits = User.where(role: User::Pandit).includes(:pandit)
-
-	end
+# 	def index
+# # @pandits = Pandit.all.includes(:user)
+#
+# 		# @pandits = User.where(role: User::Pandit).includes(:pandit)
+#
+# 	end
 
 	def show
-		@pandit = Pandit.find(params[:id])
+		@pandit = User.find(params[:id])
 	end
 
-	def getpandits
-#raise params.inspect
-		@pandits = Pandit.all.includes(:user)
+	def index
+		# raise params.inspect
+		@pandits = User.where(role: User::Pandit).paginate(:page => params[:page], :per_page => 2)
+    # Post.paginate(:page => params[:page])
 		#Item.all.includes(:categories)
 		#@pandits = User.where(role: User::Pandit).includes(:pandit)
 
        #@users = User.all.paginate(:page=> params[:page], :per_page=>3)
 	   if params[:commit]=="search"
 	# raise params.inspect
-        
+
          #@msg="sdfa"
 	  name=params[:deals]
 	  city=params[:city]
 	  state=params[:state]
-	  page= params[:page] 
+	  page= params[:page]
 	  per_page=6
-	  
+
 	@pandit = Sunspot.search(Pandit) do
 	    if name.present?
 	      with(:deals, deals)
-	    end  
+	    end
 	    if city.present?
 	      with(:city, city)
 	    end
@@ -42,11 +43,11 @@ class PanditsController < ApplicationController
 
 
 	  end.results
-     
-		
+
+
 	   #paginate :page => page, :per_page => per_page
 	  end
-       
+
 
 
 
