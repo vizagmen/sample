bfcbaae2093a5f7f4c2d8bc952d87ea67145f38d
VIZAG MEN <vizagcity2012@gmail.com>
2016-11-09 21:59:32 +0530

    Prathuish - code modified

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@ class DashboardController < ApplicationController
 	# before_action :is_admin?
  	# layout 'admin'
   def index
+    
   	@references_count  = User.where(reference_no: current_user.serial_no).count
     @refered_users = User.where(reference_no: current_user)
 
